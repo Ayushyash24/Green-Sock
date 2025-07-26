20b3ca7ac8f7da2fd717cb6d5a6fa9f60e1100fb
Ayush Yash Dwivedi <dwivediasd1020@gmail.com>
2025-07-26 11:04:12 +0530

    Made loader successfully

--- Final_Project/index.js
+++ Final_Project/index.js
@@ -1,11 +1,24 @@
-gsap.from(".line h1",{
+// gsap.from(".line h1",{
+//     y:350,
+//     stagger:0.25,
+//     duration:0.6,
+//     delay:0.5,
+// })
+
+
+var tl = gsap.timeline()
+tl.from(".line h1",{
     y:350,
     stagger:0.25,
     duration:0.6,
-    delay:0.5,
+    delay:0.5
 })
 
-var h5timer =document.querySelector("#line1-part1 h5");
+
+tl.from("#line1-part1 ",{
+    opacity:0,
+    onStart:function(){
+        var h5timer =document.querySelector("#line1-part1 h5");
 var grow=0;
 setInterval(function(){
     if(grow<100){
@@ -17,11 +30,34 @@ setInterval(function(){
     }
 },45)
 
+    }
+
+})
 
 
-var tl = gsap.timeline()
+tl.to( ".line h2",{
+    animationName: "anime",
+    opacity:1,
+
+})
+
+
 tl.to("#loader",{
     opacity:0,
-    duration: 0.4,
+    duration: 0.2,
     delay:4
+})
+
+tl.from("#page1",{
+    // delay:0.1,
+    duration:0.5,
+    y:1000,
+    opacity:0,
+    ease:Power4,
+
+})
+
+
+tl.to("#loader",{
+    display:none,
 })
